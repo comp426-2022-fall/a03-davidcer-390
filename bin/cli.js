813f83d4c85ee2e5a4bc69562c6ef171a34ab024
David Cervantes <davidcer@live.unc.edu>
2022-11-28 20:04:50 -0500

    first try

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { roll } from "../lib/roll.js";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2))
+
+const sides = args.sides ? args.sides : 6;
+const dice = args.dice ? args.dice : 2;
+const rolls = args.rolls ? args.rolls : 1;
+
+console.log(roll(sides, dice, rolls))
+
