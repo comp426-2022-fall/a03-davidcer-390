630198fb89c8399821ba6c6f992d23e9605e603b
David Cervantes <davidcer@live.unc.edu>
2022-11-28 20:08:02 -0500

    stringify json before outputing

--- bin/cli.js
+++ bin/cli.js
@@ -9,5 +9,5 @@ const sides = args.sides ? args.sides : 6;
 const dice = args.dice ? args.dice : 2;
 const rolls = args.rolls ? args.rolls : 1;
 
-console.log(roll(sides, dice, rolls))
+console.log(JSON.stringify(roll(sides, dice, rolls)))
 
